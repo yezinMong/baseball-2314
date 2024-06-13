4619c0535056f10a1d28fd1a3b266587d301a4e2
yezin.kim <yezin.kim@samsung.com>
2024-06-13 14:49:37 +0900

    [feature] 60

--- test_game.py
+++ test_game.py
@@ -38,4 +38,8 @@ class TestGame(TestCase):
 
     def test_return_solve_result_if_unmatched_number(self):
         self.generage_question("123")
-        self.assert_matched_number(self.game.guess("456"), False, 3, 0)
+        self.assert_matched_number(self.game.guess("456"), False, 0, 0)
+
+    def test_return_solve_result_if_some_matched_number(self):
+        self.generage_question("123")
+        self.assert_matched_number(self.game.guess("120"), False, 2, 0)
